dca74092fb749af6e1aefd46df27e42e70627ac4
MalakMukhtarli <malakem@code.edu.az>
2021-02-06 19:53:42 +0400

    Slider section is done

--- Fusion.az/Fusion.az/wwwroot/assets/js/main.js
+++ Fusion.az/Fusion.az/wwwroot/assets/js/main.js
@@ -1,1 +1,52 @@
 ﻿
+  // slider-in scale-nin artmasi
+
+$(document).ready(function () {
+
+        $(".slider").owlCarousel(
+            {
+                items: 1,
+                loop: true,
+                autoplay: true
+            }
+        );
+
+
+    let count = 1;
+
+    $(".owl-item.active")
+        .children("img")
+        .css({ transform: "scale(1.1)", transition: "10s" });
+
+    //$("#carouselExampleFade").on("slide.bs.carousel", function () {
+    //    if (count == 1) {
+    //        $(".owl-item.active").children("img").css({ transform: "scale(1)" });
+
+    //        $(".text h1").addClass("animate__fadeInDownBig");
+    //        $(".text h2").addClass("animate__fadeInRightBig");
+    //        $(".text p").addClass("animate__fadeInUpBig");
+
+    //        setTimeout(() => {
+    //            $(".owl-item")
+    //                .next()
+    //                .children("img")
+    //                .css({ transform: "scale(1.1)", transition: "10s" });
+    //        }, 1000);
+
+    //        count = 0;
+    //    } else {
+    //        $(".owl-item.active").children("img").css({ transform: "scale(1)" });
+
+    //        $(".text h1").addClass("animate__fadeInDownBig");
+    //        $(".text h2").addClass("animate__fadeInRightBig");
+    //        $(".text p").addClass("animate__fadeInUpBig");
+
+    //        setTimeout(() => {
+    //            $(".owl-item.active")
+    //                .children("img")
+    //                .css({ transform: "scale(1.1)", transition: "10s" });
+    //        }, 1000);
+    //        count++;
+    //    }
+    //});
+});
