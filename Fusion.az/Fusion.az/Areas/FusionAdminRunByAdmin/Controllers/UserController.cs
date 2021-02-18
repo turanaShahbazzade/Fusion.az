3bb4f1e66e2895f3f7fc7dec66efec02399490ea
NahidaM <turaneshahbazzade@gmail.com>
2021-02-18 13:55:41 +0400

    Identity has been totally done and the UserController in the Admin panel has also been created. Now the admin can see the list of users in the User Table

---
+++ Fusion.az/Fusion.az/Areas/FusionAdminRunByAdmin/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fusion.az.Extentions;
+using Fusion.az.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fusion.az.Areas.FusionAdminRunByAdmin.Controllers
+{ 
+    [Area("FusionAdminRunByAdmin")]
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            List<AppUser> users = _userManager.Users.Where(u=>u.isDeleted==false).ToList();  
+            return View(users);
+        }
+    }
+}
